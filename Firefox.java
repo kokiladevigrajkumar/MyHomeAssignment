85eb30b617e0199448bfcceadf037494c2e3829b
kokiladevigrajkumar <g.kokiladevi@gmail.com>
2024-09-16 15:12:57 +0530

    Add files via upload
    week1day1classAssignment

---
+++ Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f;
+		System.out.println("The browser version value is: "+browserVersion);
+		String browserName = "firefox";
+		System.out.println("browser name is: "+browserName);
+		boolean isVariable = true;
+		System.out.println("boolean values is: "+isVariable);
+		int releaseYear = 1998;
+		System.out.println("Release year value is: "+releaseYear);
+		char browserLogo ='f';
+		System.out.println("The browser logo is: "+browserLogo);
+	}
+
+}
